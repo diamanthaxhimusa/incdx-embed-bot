cc369049743012ed654053d22f0611fd59ca9de0
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2019-04-02 14:20:38 +0200

    Documentation, bot open button, design animations

--- incdx-bot.js
+++ incdx-bot.js
@@ -13,26 +13,38 @@ window['addHtmlString'] = function(str) {
   node.innerHTML = str;
   document.body.appendChild(node);
 };
-
+/**
+ * @name Incodeks Bot
+ * @constructor
+ * @returns {Object} methods   public functions
+ * @example
+ * incodeksBot.init({
+ *   userToken: "",
+ *   microphone: true,
+ *   messageColor: "blue",
+ *   textColor: "white"
+ * });
+ */
 var incodeksBot = (function () {
 	'use strict';
 
 	// Create the methods object
   var methods = {};
   
-  /*
+  /**
    * Init Bot
+   * @function
    * @param {Object}  options                         Options of BOT
    * @param {Boolean} [options.microphone=true]       Use the microphone          [optional]
    * @param {String}  [options.messageColor=#a5d175]  Color of the messsage box   [optional]
    * @param {String}  [options.textColor=#fff]        Color of the meesage text   [optional]
    */
   methods.init = function(options) {
-    var incdxHtml = '<div id="preloader" style="opacity: 0; display: none;"> <noscript> <h1>This application does not work without javascript</h1> </noscript> <div class="logo"></div></div><div class="incdx-bot"> <div class="incdx-bot_header"> <div class="incdx-bot_header-wrapper"> <div class="incdx-bot_header_header-agent-name">TEST BOT</div></div></div><div class="incdx-bot_result" id="resultWrapper"> <table class="incdx-bot_result-table"> <tbody> <tr> <td id="incdx-result"></td></tr><tr id="incdx-w-l"></tr></tbody> </table> </div><div class="clearfix"></div><div class="incdx-bot-form_input"> <form id="incdx-bot-form"> <input type="text" name="q" autofocus id="incdx-bot-query" placeholder="Text something..." autocomplete="off" > <button id="incdx-bot-submit" type="submit"> <i class="fa fa-send" id="send"></i> </button> <div id="incdx-bot-mic"> <i class="fa fa-microphone" id="incdx-bot-mic-icon"></i> <div id="mic-active-container"> <span style="animation: sound-1 1.4s infinite"></span> <span style="animation: sound-2 1.4s 0.25s infinite"></span> <span style="animation: sound-1 1.4s 0.10s infinite"></span> <span style="animation: sound-2 1.4s 0.15s infinite"></span> </div></div></form> </div></div>';
+    var incdxHtml = '<button id="incdx-bot-open" > <i class="fa fa-comment fa-2x" aria-hidden="true"></i> <i class="fa fa-close fa-2x" aria-hidden="true"></i> </button><div class="incdx-bot bot-hidden" id="incdx-bot-wrapper"><div class="incdx-bot_header"><div class="incdx-bot_header-wrapper"><div class="incdx-bot_header_header-agent-name">TEST BOT</div></div></div><div class="incdx-bot_result" id="resultWrapper"><table class="incdx-bot_result-table"><tbody><tr><td id="incdx-result"></td></tr><tr id="incdx-w-l"></tr></tbody></table></div><div class="clearfix"></div><div class="incdx-bot-form_input"><form id="incdx-bot-form"> <input type="text" name="q" autofocus id="incdx-bot-query" placeholder="Text something..." autocomplete="off" > <button id="incdx-bot-submit" type="submit"> <i class="fa fa-send" id="send"></i> </button><div id="incdx-bot-mic"> <i class="fa fa-microphone" id="incdx-bot-mic-icon"></i><div id="mic-active-container"> <span style="animation: sound-1 1.4s infinite"></span> <span style="animation: sound-2 1.4s 0.25s infinite"></span> <span style="animation: sound-1 1.4s 0.10s infinite"></span> <span style="animation: sound-2 1.4s 0.15s infinite"></span></div></div></form></div></div>';
     // Get message color from options, or set the default one #a5d175.
     var botMessageColor = options.messageColor || "#a5d175";
     var textColor = options.textColor || "#fff";
-    var incdxStyle = `body{padding:0;margin:0}.incdx-bot .clearfix{clear:both}.incdx-bot .incdx-bot-message-card{display:inline-block;padding:15px 25px;border-radius:3px;border:1px solid #eee;margin-bottom:5px;font-size:16px;clear:both}.incdx-bot .incdx-message-user-request{background-color:#efefef;float:left;margin-right:15px;margin-top:15px;margin-left:15px}.incdx-bot .incdx-message-server-response{color:${textColor};background-color:${botMessageColor};float:right;margin-top:15px;margin-right:15px;margin-left:15px}.incdx-bot .incdx-message-server-response.server-response-error{background-color:#f76949}.incdx-bot .incdx-bot_result-table td{vertical-align:bottom;display:table-cell}.incdx-bot .incdx-bot_result-table{height:100%;min-height:100%;width:100%}#incdx-w-l{height:0}.incdx-bot .incdx-bot_result{overflow-y:auto;background:#fff;position:fixed;top:80px;bottom:55px;width:100%}.incdx-bot .incdx-bot_header{min-height:80px;height:80px;overflow:hidden;position:fixed;top:0;width:100%;background-color:#2b303e;display:table}.incdx-bot_header-wrapper{height:100%;display:flex;justify-content:center;align-items:center;color:${textColor};font-size:1.2em}.incdx-bot .incdx-bot-form_input{position:fixed;bottom:0;height:55px;border-top:1px solid #d3d3d3;background-color:#fff;width:100%}.incdx-bot #incdx-bot-form{display:block;margin-left:15px;margin-right:110px}.incdx-bot #incdx-bot-form #incdx-bot-query{width:100%;border:0;font-size:16px;font-weight:300;margin:0;height:55px}.incdx-bot #incdx-bot-form #incdx-bot-submit,.incdx-bot #incdx-bot-mic{position:absolute;font-size:20px;width:54px;height:54px;top:0;bottom:0;cursor:pointer;text-align:center;line-height:30px;line-height:54px;background:#fff;color:#b7bbc4;border:none;display:flex;justify-content:center;align-items:center}.incdx-bot #incdx-bot-form #incdx-bot-submit{right:54px}.incdx-bot #incdx-bot-mic{right:0}#incdx-bot-mic.active #incdx-bot-mic-icon{display:none}#incdx-bot-mic.active #mic-active-container{display:flex}#mic-active-container{justify-content:space-evenly;width:54px;height:54px;position:relative;padding:0 7px;display:none}#mic-active-container span{width:6px;height:6px;border-radius:50%;margin:auto;top:0;bottom:0;transition:all .3s}#mic-active-container span:first-child{background:#4285f4}#mic-active-container span:nth-child(2){background:#db4437}#mic-active-container span:nth-child(3){background:#f4b400}#mic-active-container span:nth-child(4){background:#0f9d58}@keyframes sound-1{0%{height:52px;border-radius:30px}10%{height:40px;border-radius:30px}20%{height:45px;border-radius:30px}25%{height:35px;border-radius:30px}30%{height:52px;border-radius:30px}40%{height:37px;border-radius:30px}50%{height:34px;border-radius:30px}60%{height:32px;border-radius:30px}70%{height:27px;border-radius:30px}80%{height:32px;border-radius:30px}85%{height:27px;border-radius:30px}90%{height:32px;border-radius:30px}95%{height:27px;border-radius:30px}100%{height:15px;border-radius:50%}}@keyframes sound-2{0%{height:42px;border-radius:30px}20%{height:32px;border-radius:30px}30%{height:42px;border-radius:30px}40%{height:32px;border-radius:30px}50%{height:31px;border-radius:30px}60%{height:30px;border-radius:30px}70%{height:29px;border-radius:30px}80%{height:30px;border-radius:30px}90%{height:29px;border-radius:30px}100%{height:15px;border-radius:50%}}.incdx-bot #incdx-bot-query:focus,.incdx-bot #incdx-bot-submit:focus{outline:0}.incdx-list-select{background-color:#fff;padding:15px;border-radius:4px;color:#3c4043}.incdx-list-select .incdx-list-select-item{padding:16px 0;border-top:2px solid #d8d8d8;cursor:pointer;min-height:100px}.incdx-list-select .incdx-list-select-title{font-size:28px;color:#3c4043;letter-spacing:0;line-height:36px;text-align:left}.incdx-list-select .incdx-list-select-item .incdx-list-select-item-title{font-size:18px;color:#202124;line-height:24px}.incdx-list-select .incdx-list-select-item .incdx-list-select-item-image{height:76px;width:76px}.incdx-list-select .incdx-list-select-item .incdx-list-select-item-image>img{max-height:100%;max-width:100%;border-radius:0}.incdx-layout,.incdx-layout-column,.incdx-layout-row{-webkit-box-sizing:border-box;box-sizing:border-box;display:-webkit-box;display:-moz-box;display:-webkit-flex;display:-ms-flexbox;display:flex}.incdx-flex{-webkit-box-flex:1;-webkit-flex:1;flex:1;-webkit-box-sizing:border-box;box-sizing:border-box}.incdx-layout-column{-webkit-flex-direction:column;flex-direction:column}.incdx-flex-70,.incdx-layout-row>.incdx-flex-70{-webkit-box-flex:1 1 100%;-webkit-flex:1 1 100%;flex:1 1 100%;max-width:70%;max-height:100%;-webkit-box-sizing:border-box;box-sizing:border-box}.incdx-list-select h2{font-size:1.5em;margin:10px 0}`;
+    var incdxStyle = `body{padding:0;margin:0}.incdx-bot .clearfix{clear:both}button#incdx-bot-open{position:fixed;bottom:0;right:0;width:60px;height:60px;color:#fff;background-color:#0360a5;background-position:center center;background-repeat:no-repeat;box-shadow:12px 15px 20px 0 rgba(46, 61, 73, 0.15);border:0;border-radius:50%;cursor:pointer;margin:0px 40px 30px 0px;outline:0 !important;z-index:10002;display:flex;justify-content:center;align-items:center}button#incdx-bot-open i{position:absolute}button#incdx-bot-open i:first-child{transition:opacity 0.3s, transform 0.3s}button#incdx-bot-open i:nth-child(2){transition:opacity 0.3s, transform 0.3s;transform:rotate(-180deg) scale(0.5);opacity:0}button#incdx-bot-open.close i:first-child{transform:rotate(180deg) scale(0.5);opacity:0}button#incdx-bot-open.close i:nth-child(2){transform:rotate(0deg) scale(1);opacity:1}#incdx-bot-wrapper{position:absolute;width:377px;height:500px;border-radius:16px;overflow:hidden;right:40px;opacity:1;bottom:calc(2 * 30px + 52px);box-shadow:3px 3px 20px -10px black;transition:all 0.2s linear}#incdx-bot-wrapper.bot-hidden{height:0;width:0;opacity:0;bottom:60px;right:40px;z-index:10001}.incdx-bot .incdx-bot-message-card{display:inline-block;padding:15px 25px;border-radius:3px;border:1px solid #eee;margin-bottom:5px;font-size:16px;clear:both;max-width:250px}.incdx-bot .incdx-message-user-request{background-color:#efefef;float:left;margin-right:15px;margin-top:15px;margin-left:15px}.incdx-bot .incdx-message-server-response{color:#fff;background-color:#a5d175;float:right;margin-top:15px;margin-right:15px;margin-left:15px}.incdx-bot .incdx-message-server-response.server-response-error{background-color:#f76949}.incdx-bot .incdx-bot_result-table td{vertical-align:bottom;display:table-cell}.incdx-bot .incdx-bot_result-table{height:100%;min-height:100%;width:100%}#incdx-w-l{height:0px}.incdx-bot .incdx-bot_result{overflow-y:auto;background:white;position:absolute;top:65px;bottom:55px;width:100%}.incdx-bot .incdx-bot_header{min-height:65px;height:65px;overflow:hidden;position:absolute;top:0;width:100%;background-color:#2b303e;display:table}.incdx-bot_header-wrapper{height:100%;display:flex;justify-content:center;align-items:center;color:#fff;font-size:1.2em}.incdx-bot .incdx-bot-form_input{position:absolute;bottom:0;height:55px;border-top:1px solid lightgray;background-color:white;width:100%}.incdx-bot #incdx-bot-form{display:block;margin-left:15px;margin-right:110px;height:100%}.incdx-bot #incdx-bot-form #incdx-bot-query{width:100%;border:0;font-size:16px;font-weight:300;margin:0;height:100%;padding:0}.incdx-bot #incdx-bot-form #incdx-bot-submit, .incdx-bot #incdx-bot-mic{position:absolute;font-size:20px;width:54px;height:54px;top:0;bottom:0;cursor:pointer;text-align:center;line-height:30px;line-height:54px;background:#fff;color:#b7bbc4;border:none;display:flex;justify-content:center;align-items:center}.incdx-bot #incdx-bot-form #incdx-bot-submit{right:54px}.incdx-bot #incdx-bot-mic{right:0}#incdx-bot-mic.active #incdx-bot-mic-icon{display:none}#incdx-bot-mic.active #mic-active-container{display:flex}#mic-active-container{justify-content:space-evenly;width:54px;height:54px;position:relative;padding:0px 7px;display:none}#mic-active-container span{width:6px;height:6px;border-radius:50%;margin:auto;top:0;bottom:0;transition:all .3s}#mic-active-container span:first-child{background:#4285f4}#mic-active-container span:nth-child(2){background:#db4437}#mic-active-container span:nth-child(3){background:#f4b400}#mic-active-container span:nth-child(4){background:#0f9d58}@keyframes sound-1{0%{height:52px;border-radius:30px}10%{height:40px;border-radius:30px}20%{height:45px;border-radius:30px}25%{height:35px;border-radius:30px}30%{height:52px;border-radius:30px}40%{height:37px;border-radius:30px}50%{height:34px;border-radius:30px}60%{height:32px;border-radius:30px}70%{height:27px;border-radius:30px}80%{height:32px;border-radius:30px}85%{height:27px;border-radius:30px}90%{height:32px;border-radius:30px}95%{height:27px;border-radius:30px}100%{height:15px;border-radius:50%}}@keyframes sound-2{0%{height:42px;border-radius:30px}20%{height:32px;border-radius:30px}30%{height:42px;border-radius:30px}40%{height:32px;border-radius:30px}50%{height:31px;border-radius:30px}60%{height:30px;border-radius:30px}70%{height:29px;border-radius:30px}80%{height:30px;border-radius:30px}90%{height:29px;border-radius:30px}100%{height:15px;border-radius:50%}}.incdx-bot #incdx-bot-submit:focus, .incdx-bot #incdx-bot-query:focus{outline:none}.incdx-list-select{background-color:#fff;padding:15px;border-radius:4px;color:#3c4043}.incdx-list-select .incdx-list-select-item{padding:16px 0;border-top:2px solid #d8d8d8;cursor:pointer;min-height:100px}.incdx-list-select .incdx-list-select-title{font-size:28px;color:#3c4043;letter-spacing:0;line-height:36px;text-align:left}.incdx-list-select .incdx-list-select-item .incdx-list-select-item-title{font-size:18px;color:#202124;line-height:24px}.incdx-list-select .incdx-list-select-item .incdx-list-select-item-image{height:76px;width:76px}.incdx-list-select .incdx-list-select-item .incdx-list-select-item-image>img{max-height:100%;max-width:100%;border-radius:0px}.incdx-layout,.incdx-layout-column,.incdx-layout-row{-webkit-box-sizing:border-box;box-sizing:border-box;display:-webkit-box;display:-moz-box;display:-webkit-flex;display:-ms-flexbox;display:flex}.incdx-flex{-webkit-box-flex:1;-webkit-flex:1;flex:1;-webkit-box-sizing:border-box;box-sizing:border-box}.incdx-layout-column{-webkit-flex-direction:column;flex-direction:column}.incdx-flex-70,.incdx-layout-row>.incdx-flex-70{-webkit-box-flex:1 1 100%;-webkit-flex:1 1 100%;flex:1 1 100%;max-width:70%;max-height:100%;-webkit-box-sizing:border-box;box-sizing:border-box}.incdx-list-select h2{font-size:1.5em;margin:10px 0}`;
     window.addStyleString(incdxStyle);
     window.addHtmlString(incdxHtml);
     
@@ -42,6 +54,10 @@ var incodeksBot = (function () {
       var botMic = document.getElementById("incdx-bot-mic");
       botMic.classList.add("hidden");
     }
+    // Add Event Listener on button to open the Bot popup
+    var openBotButton = document.getElementById('incdx-bot-open');
+    openBotButton.addEventListener("click", openBot);
+
     var form = document.getElementById('incdx-bot-form');
     form.addEventListener("submit", function (e) {
       e.preventDefault();
@@ -102,12 +118,19 @@ var incodeksBot = (function () {
     }
   }
 
+  var openBot = function() {
+    var incdxBotWrapper = document.getElementById("incdx-bot-wrapper");
+    incdxBotWrapper.classList.toggle("bot-hidden");
+    this.classList.toggle("close");
+  }
+
   /*
    * Send Message
    * @param {String} message    Message that we send to bot to get intents back and create answers
    */
   var sendMessage = function (message) {
-    var urlToIncdxBot = 'http://localhost:5000/listselectionsample/us-central1/helloWorld';
+    // test url http://localhost:5000/listselectionsample/us-central1/botFunction
+    var urlToIncdxBot = 'http://localhost:5000/listselectionsample/us-central1/botFunction';
     var botResultContainer = document.getElementById("incdx-result");
     var botLoadingContainer = document.getElementById("incdx-w-l");
     var resultWrapper = document.getElementById("resultWrapper");
@@ -217,8 +240,11 @@ var incodeksBot = (function () {
       })
       .catch(error => {
         console.log(error)
-        botMessage.classList.add("server-response-error");
+        if(loadingMessage) loadingMessage.remove();
+        var botMessage = document.createElement("div");
+        botMessage.classList.add("incdx-message-server-response", "server-response-error");
         botMessage.innerText = "Sorry, it seemed like there was an error during request.";
+        botResultContainer.appendChild(botMessage);
         resultWrapper.scrollTop = resultWrapper.scrollHeight;
       });
   }
